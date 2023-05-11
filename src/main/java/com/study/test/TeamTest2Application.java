2d178110f342dd6d50f185c40693b22fdf6ada9f
yeonghongsim <a93200551@gmail.com>
2023-05-11 11:59:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/study/test/TeamTest2Application.java
@@ -0,0 +1,13 @@
+package com.study.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/test/TeamTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
